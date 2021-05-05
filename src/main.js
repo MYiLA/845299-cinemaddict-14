f30435bc6f358b2153dc6c6016193ed9dd02931f
Дюбанова Мария Борисовна <dyubanova.mb@esphere.ru>
2021-05-06 02:53:39 +0300

    add api module

--- src/main.js
+++ src/main.js
@@ -4,16 +4,28 @@ import { films, commentsData } from './mock/data.js';
 import FilmsModel from './model/films.js';
 import СommentsModel from './model/comments.js';
 import FilterModel from './model/filter.js';
-
 import ContentPresenter from './presenter/content.js';
 import FilterPresenter from './presenter/filter.js';
 import MenuView from './view/menu.js';
 import ProfileView from './view/profile.js';
 import MoviesCountView from './view/movies-count.js';
+import Api from './api.js';
+
+const AUTHORIZATION = 'Basic htmlAcademy777Myila';
+const END_POINT = 'https://14.ecmascript.pages.academy/cinemaddict';
 
 const filmsModel = new FilmsModel();
 filmsModel.setFilms(films);
 const commentsModel = new СommentsModel(commentsData);
+const api = new Api(END_POINT, AUTHORIZATION);
+
+api.getFilms().then((films) => {
+  console.log(films);
+  // Есть проблема: cтруктура объекта похожа, но некоторые ключи называются иначе,
+  // а ещё на сервере используется snake_case, а у нас camelCase.
+  // Можно, конечно, переписать часть нашего клиентского приложения, но зачем?
+  // Есть вариант получше - паттерн "Адаптер"
+});
 
 const viewedCount = getFilmPropertyCount(filmsModel.getFilms(), 'isViewed');
 
